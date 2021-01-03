40e170caf2eb25971e8a9f9603d4d66b4981b084
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-01-03 23:56:21 +0530

    Django 3.1.4 Version Updated

--- app1/admin.py
+++ app1/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from .models import *
-
-
- #Register your models here.
-
+# Register your models here.
 admin.site.register(Rented_Bikes)
 admin.site.register(Available_Bikes)
--- app1/models.py
+++ app1/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Rented_Bikes(models.Model):
-    username = models.CharField(max_length=20,null=True)
-    location = models.CharField(max_length=20,null=True)
-    start_time = models.TimeField(auto_now=False,auto_now_add=False,null=True)
-    end_time = models.TimeField(auto_now=False,auto_now_add=False,null=True)
-    bike = models.CharField(max_length=20,null=True)
+    username = models.CharField(max_length=20, null=True)
+    location = models.CharField(max_length=20, null=True)
+    start_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
+    end_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
+    bike = models.CharField(max_length=20, null=True)
     amount = models.IntegerField(null=True)
     otp = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.username+" - "+self.bike
+        return self.username + " - " + self.bike
 
 
 class Available_Bikes(models.Model):
-    name = models.CharField(max_length=20,null=True)
-    location = models.CharField(max_length=20,null=True)
+    name = models.CharField(max_length=20, null=True)
+    location = models.CharField(max_length=20, null=True)
     number = models.IntegerField(null=True)
 
     def __str__(self):
