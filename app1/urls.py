cdbf3184bd2809767ae5d12469248e6f9a6fced3
Avi Kathuria <avikathuria21@gmail.com>
2021-02-07 16:02:17 +0530

    #51 Frontend Integration - Favicons
    Added Favicon for website

--- app1/urls.py
+++ app1/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from app1 import views
 
 urlpatterns = [
-    
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
